f92b5c63ce9396c1a9921483038f20625df1162e
azure desktop <aden7x@gmail.com>
2019-04-21 19:52:15 -0700

    Add timestamps to messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,18 +12,20 @@ const $messages = document.querySelector('#messages');
 const messageTemplate = document.querySelector('#message-template').innerHTML;
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
 
-socket.on('locationMessage', (url) => {
-    console.log(url);
-    const html = Mustache.render(locationMessageTemplate, {
-        url
+socket.on('message', (msg) => {
+    console.log(msg);
+    const html = Mustache.render(messageTemplate, {
+        message: msg.text,
+        createdAt: moment(msg.createdAt).format('h:mm a')
     });
     $messages.insertAdjacentHTML('beforeend', html);
 });
 
-socket.on('message', (msg) => {
-    console.log(msg);
-    const html = Mustache.render(messageTemplate, {
-        message: msg
+socket.on('locationMessage', (message) => {
+    console.log(message);
+    const html = Mustache.render(locationMessageTemplate, {
+        url: message.url,
+        createdAt: moment(message.createdAt).format('h:mm a')
     });
     $messages.insertAdjacentHTML('beforeend', html);
 });
