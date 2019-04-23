0a2ddb0ed59fc2ec1f0cf276d2e54725b0bf2e8f
azure desktop <aden7x@gmail.com>
2019-04-22 20:14:52 -0700

    add styles to msgs and add rooms

--- src/index.js
+++ src/index.js
@@ -17,8 +17,13 @@ app.use(express.static(publicDirectoryPath));
 io.on('connection', (socket) => {
     console.log('New websocket connection');
 
-    socket.emit('message', generateMessage('Welcome'));
-    socket.broadcast.emit('message', generateMessage('A new user has joined'));
+    socket.on('join', ({username, room}) => {
+       socket.join(room) 
+
+       socket.emit('message', generateMessage('Welcome'));
+       socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined`));
+   
+    });
 
     socket.on('sendMessage', (msg, callback) => {
         const filter = new Filter();
@@ -27,7 +32,7 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed');
         }
 
-        io.emit('message',generateMessage(msg));
+        io.to('blue').emit('message',generateMessage(msg));
         callback();
     });
 
