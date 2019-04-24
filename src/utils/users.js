38a2f7dd1be67dd544ae9bd3f25aec08009af2e9
azure desktop <aden7x@gmail.com>
2019-04-23 20:44:36 -0700

    add users

---
+++ src/utils/users.js
@@ -0,0 +1,63 @@
+const users = [];
+
+
+const addUser = ({id, username, room}) => {
+    username = username.trim().toLowerCase();
+    room = room.trim().toLowerCase();
+
+    //validate the data
+    if(!username || !room) {
+        return {
+            error: 'Username and room are required!'
+        }
+    }
+
+    //check for existing user
+    const existingUser = users.find((user) => {
+        return user.room === room && user.username === username;
+    })
+
+    if(existingUser) {
+        return {
+            error: "Username is in use!"
+        }
+    }
+
+    //Store user
+    const user = {id, username, room};
+    users.push(user);
+    return {user};
+}
+
+const removeUser = (id) => {
+    const index = users.findIndex((user) => {
+        return user.id === id;
+    });
+    
+    if(index !== -1) {
+        return users.splice(index, 1)[0];
+    }
+}
+
+const getUser = (id) => {
+    const user = users.find((user) => {
+        return user.id === id;
+    })
+
+    return user;
+}
+
+const getUsersInRoom = (room) => {
+    const roomUsers = users.filter((user) => {
+        return user.room === room;
+    })
+
+    return roomUsers;
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
+}
